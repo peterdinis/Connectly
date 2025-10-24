68fcc352d2286efba6b693578c04ffac1cdeb4c3
peterdinis <petter1@pobox.sk>
2025-10-24 17:22:52 +0200

    feat: Create dashboard links page

---
+++ app/dashboard/links/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/components/ui/spinner";
+import { FC } from "react";
+
+const Loading: FC = () => {
+    return <Spinner />
+}
+
+export default Loading
---
+++ app/dashboard/links/page.tsx
@@ -0,0 +1,8 @@
+import LinksWrapper from "@/components/dashboard/links/LinksWrapper";
+import { NextPage } from "next";
+
+const DashboardLinksPage: NextPage = () => {
+    return <LinksWrapper />;
+}
+
+export default DashboardLinksPage
