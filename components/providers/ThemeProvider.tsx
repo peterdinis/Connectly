a0dae105ce7afbfaae1ff8fca65005ab6a47ee0a
peterdinis <petter1@pobox.sk>
2025-10-24 14:05:46 +0200

    infra: Enable light and dark mode

---
+++ components/providers/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ComponentProps, useState, useEffect } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
